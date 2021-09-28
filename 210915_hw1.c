3f285d7567b06bd3da9d437200dcfb5132863ef7
height190 <minhyuk01234@gmail.com>
2021-09-28 22:55:56 +0900

    210915_hw1.c commit

--- 210915_hw1.c
+++ 210915_hw1.c
@@ -1,21 +1,11 @@
 #include <stdio.h>
-
 int main(void) {
 
-	char ch;
-	int num;
+    char ch;
 
-	printf("문자 입력: ");
-	scanf_s("%c", &ch);
+    printf("문자 입력: ");
+    scanf_s("%c", &ch);
+    printf("%c의 아스키코드 값은 %d입니다", ch, ch);
 
-	if (ch == 'A') {
-		num = 65;
-	}
-	else if (ch == 't') {
-		num = 116;
-	}
-
-	printf("%c문자의 아스키 코드 값은 %d입니다", ch, num);
-
-	return 0;
+    return 0;
 }
